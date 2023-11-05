7e37d248250d71fc9d7f4535967476b6f1b162d2
Avinash <avinash.pakala93@gmail.com>
2023-11-04 20:37:22 -0700

    alert dashboard done

--- client/src/components/alerts/alert.js
+++ client/src/components/alerts/alert.js
@@ -1,94 +1,38 @@
-import React, { useState, useEffect } from "react"
-import Row from "react-bootstrap/Row"
-import Form from "react-bootstrap/Form"
-import NavBarLoggedIn from "../Navbar/NavBarLoggedIn"
-import Col from "react-bootstrap/Col"
-import LeftNavBar from "../LeftNavBar/LeftNavBar"
-import Button from "react-bootstrap/Button"
-import axios from "axios"
-import { Modal } from "react-bootstrap"
-import AlertTable from './alertTable';
-import {
-  MDBBadge,
-  MDBBtn,
-  MDBTable,
-  MDBTableHead,
-  MDBTableBody,
-} from "mdb-react-ui-kit"
-
+import React, { useState, useEffect } from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import LeftNavBar from "../LeftNavBar/LeftNavBar";
+import AlertTable from './AlertTable';
+import axios from "axios";
+import NavBarLoggedIn from "../Navbar/NavBarLoggedIn";
 
 function Alert() {
-
-  const dummyAlerts = [
-    {
-      id: 1,
-      priority: 'High',
-      detection: 'Intrusion',
-      createdTime: '2022-05-01T14:30:00Z',
-    },
-    {
-      id: 2,
-      priority: 'Medium',
-      detection: 'Intrusion',
-      createdTime: '2022-05-02T08:15:00Z',
-    },
-    {
-      id: 3,
-      priority: 'Low',
-      detection: 'Motion',
-      createdTime: '2022-05-03T16:45:00Z',
-    },
-    {
-      id: 4,
-      priority: 'High',
-      detection: 'Illegal Dumping',
-      createdTime: '2022-05-04T21:20:00Z',
-    },
-    {
-      id: 5,
-      priority: 'Low',
-      detection: 'Intrusion',
-      createdTime: '2022-05-05T12:00:00Z',
-    },
-    {
-      id: 6,
-      priority: 'Medium',
-      detection: 'Illegal Dumping',
-      createdTime: '2022-05-06T17:30:00Z',
-    },
-  ];
-  
-
-  const [locations, setLocations] = useState([])
-  const [startDate, setStartDate] = useState("")
-  const [endDate, setEndDate] = useState("")
-  const [cameraId, setCameraId] = useState("")
-  const [buildingName, setBuildingName] = useState("")
-  const [data, setData] = useState([])
+  const [alerts, setAlerts] = useState([]);
+  const [locations, setLocations] = useState("");
 
   useEffect(() => {
     async function fetchLocations() {
-      const response = await axios.get("http://localhost:5001/building/getList")
-      //console.log(response.data.buildingNames);
-      setLocations(response.data.buildingNames)
+      try {
+        const response = await axios.get("http://localhost:5001/building/getList");
+        setLocations(response.data.buildingNames);
+      } catch (error) {
+        console.error("Error fetching locations:", error);
+      }
     }
-    fetchLocations()
-  }, [])
 
-  const handleSubmit = (event) => {
-    event.preventDefault()
-    axios
-      .get("http://localhost:5001/videoData", {
-        params: {
-          startDate: startDate,
-          endDate: endDate,
-          cameraId: cameraId,
-          buildingName: buildingName,
-        },
-      })
-      .then((response) => setData(response.data.videoData))
-      .catch((error) => console.log(error))
-  }
+    async function fetchAlerts() {
+      try {
+        const response = await axios.get("http://localhost:5001/alert"); // Updated to the new alert endpoint
+        setAlerts(response.data.alerts);
+      } catch (error) {
+        console.error("Error fetching alerts:", error);
+      }
+    }
+
+    fetchLocations();
+    fetchAlerts();
+  }, []);
+
   return (
     <>
       <NavBarLoggedIn />
@@ -101,7 +45,6 @@ function Alert() {
           lg={10}
           style={{
             width: 800,
-            // marginLeft: 100,
             paddingLeft: 60,
             paddingRight: 60,
           }}
@@ -112,12 +55,12 @@ function Alert() {
               {" "}
               <h4>Alert Dashboard</h4>
             </Col>
-          </Row>  
-          <AlertTable alerts={dummyAlerts}/>
+          </Row>
+          <AlertTable alerts={alerts} />
         </Col>
       </Row>
     </>
-  )
+  );
 }
 
 export default Alert;
--- client/src/components/alerts/alertTable.js
+++ client/src/components/alerts/alertTable.js
@@ -1,28 +1,93 @@
-import React from 'react';
-import { Table } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Table, Form, Button } from 'react-bootstrap';
+import BootstrapSwitchButton from 'bootstrap-switch-button-react';
 
 const AlertTable = ({ alerts }) => {
+  const [filterType, setFilterType] = useState(''); // State for the type filter
+  const [filterPriority, setFilterPriority] = useState(''); // State for the priority filter
+
+  // Apply filters to the alerts
+  const filteredAlerts = alerts.filter((alert) => {
+    if (filterType && alert.type !== filterType) {
+      return false;
+    }
+    if (filterPriority && alert.priority !== filterPriority) {
+      return false;
+    }
+    return true;
+  });
+
   return (
-    <Table striped bordered hover>
-      <thead>
-        <tr>
-          <th>Alert ID</th>
-          <th>Priority</th>
-          <th>Detection</th>
-          <th>Created Time</th>
-        </tr>
-      </thead>
-      <tbody>
-        {alerts.map(alert => (
-          <tr key={alert.id}>
-            <td>{alert.id}</td>
-            <td>{alert.priority}</td>
-            <td>{alert.detection}</td>
-            <td>{alert.createdTime}</td>
+    <div>
+      <Form>
+        <Form.Group>
+          <Form.Label>Type:</Form.Label>
+          <Form.Control
+            as="select"
+            onChange={(e) => setFilterType(e.target.value)}
+            value={filterType}
+          >
+            <option value="">All</option>
+            <option value="Motion">Motion</option>
+            <option value="Sound">Sound</option>
+          </Form.Control>
+        </Form.Group>
+
+        <Form.Group>
+          <Form.Label>Priority:</Form.Label>
+          <Form.Control
+            as="select"
+            onChange={(e) => setFilterPriority(e.target.value)}
+            value={filterPriority}
+          >
+            <option value="">All</option>
+            <option value="High">High</option>
+            <option value="Medium">Medium</option>
+            <option value="Low">Low</option>
+            {/* Add more priority options as needed */}
+          </Form.Control>
+        </Form.Group>
+      </Form>
+
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Alert ID</th>
+            <th>Priority</th>
+            <th>Type</th>
+            <th>Timestamp</th>
+           
+            <th>Description</th>
+            <th>Is Read</th>
+            <th>Status</th>
           </tr>
-        ))}
-      </tbody>
-    </Table>
+        </thead>
+        <tbody>
+          {filteredAlerts.map((alert) => (
+            <tr key={alert._id}>
+              <td>{alert._id}</td>
+              <td>{alert.priority}</td>
+              <td>{alert.type}</td>
+              <td>{alert.timestamp}</td>
+             
+              <td>{alert.description}</td>
+              <td>
+                <BootstrapSwitchButton
+                  checked={alert.isRead}
+                  onlabel="Yes"
+                  offlabel="No"
+                  size="sm"
+                  onstyle="success"
+                  offstyle="danger"
+                  onChange={() => {}}
+                />
+              </td>
+              <td>{alert.status}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </div>
   );
 };
 
