03de46786afc674e0cb1538155dc8f561c085354
apparajuakhil <94913578+apparajuakhil@users.noreply.github.com>
2023-12-28 14:52:12 -0800

    fixed building name filters

--- backend/controllers/videoDataController.js
+++ backend/controllers/videoDataController.js
@@ -11,11 +11,11 @@ const getVideoDataByFilters = async (req, res, next) => {
       };
     }
     if (req.query.buildingName) {
-      const buildingIds = await Building.find({ name: new RegExp(req.query.buildingName, 'i') }).distinct('buildingId');
-      query.buildingId = { $in: buildingIds };
+      //const buildingIds = await Building.find({ name: new RegExp(req.query.buildingName, 'i') }).distinct('_id');
+      query.buildingName = req.query.buildingName;
     }
     if (req.query.cameraId) {
-      query.cameraId = Number(req.query.cameraId);
+      query.cameraId = req.query.cameraId;
     }
     console.log(query);
     const videoData = await VideoData.find(query);//.populate('buildingId').populate('cameraId');
